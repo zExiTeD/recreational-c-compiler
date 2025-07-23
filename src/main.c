ec9bd1529ab9a60d7c2f5cd9fb66717fd86d258e
core <146551153+Extinctione@users.noreply.github.com>
2025-07-24 01:58:56 +0530

    main.c

--- src/main.c
+++ src/main.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 #include "../include/lexer.h"
 
-int
-main( int argc ,char* argv[]) {
+int main(int argc,char* argv[]) {
 	FILE *filed = fopen("test/test.c","r");
 	if (filed == NULL) {
-		printf( "coudlnt open file \n");
+		printf( "couldnt open file \n");
 		return -1;
 	}
 
-	fclose(filed);
-	return 0;
+ Token token; //keep reading tokens until we hit the end of the file
+    do {
+        token = get_next_token(filed);  //call the lexer
+        printf("Token: %-12s Lexeme: %s\n", 
+               token_type_to_string(token.type),  //converting to string
+               token.lexeme);                     //print the actual text(ex "int","main","{" )
+    } while (token.type != EOF_TOKEN);  //stop when eof token is reached
+    fclose(filed);
+    return 0;
 }
