c9e64770f1a5ac5290f89585f2f91eb3689ad31c
zExiTeD <yadu9095@gmail.com>
2025-07-23 12:21:23 +0530

    [lexer started]

--- include/lexer.h
+++ include/lexer.h
@@ -1,4 +1,30 @@
 #ifndef LEXER_H
 #define LEXER_H
 
+const char* keywords[] = {
+    "auto", "break", "case", "char", "const", "continue", "default", "do",
+    "double", "else", "enum", "extern", "float", "for", "goto", "if",
+    "inline", "int", "long", "register", "restrict", "return", "short",
+    "signed", "sizeof", "static", "struct", "switch", "typedef", "union",
+    "unsigned", "void", "volatile", "while", "_Bool", "_Complex", "_Imaginary"
+};
+
+const int num_keywords = sizeof(keywords) / sizeof(keywords[0]);
+
+
+typedef enum {
+    EOF,
+    IDENTIFIER,
+    NUMBER,
+    STRING,
+    CHAR,
+    KEYWORD,
+    OPERATOR,
+    PUNCTUATION,
+    COMMENT,
+    WHITESPACE,
+    UNKNOWN
+} TokenType;
+
+
 #endif//LEXER_H
--- src/lexer.c
+++ src/lexer.c
@@ -1,1 +1,2 @@
 #include "../include/lexer.h"
+
